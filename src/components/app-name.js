139f83e19250721bd628e3893bf9ffbe3adeedcf
MykolaHlova <kolyahlova@gmail.com>
2020-04-17 14:53:57 +0300

    second part ssc

--- src/components/app-name.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const AppName = () => {
-    return <h1>My Todo List</h1>
-};
-
-export default AppName;
--- src/index.js
+++ src/index.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 
-import AppName from "./components/app-name";
+import AppHeader from "./components/app-header";
 import SearchPanel from "./components/search-panel";
 import TodoList from "./components/todo-list";
-
+import ItemStatusFilter from "./components/item-status-filter";
+import "./components/index.css";
 
 const App = () => {
+
+    const todoData = [
+        {label: 'Drink coffee', important: false, id: 0},
+        {label: 'Build awesome app', important: true, id: 1},
+        {label: 'Have a lunch', important: false, id: 2},
+    ];
+
     return (
-        <div>
-            <AppName />
-            <SearchPanel />
-            <TodoList />
+        <div className="todo-app">
+            <AppHeader toDo={1} done={3}/>
+            <div className="top-panel d-flex">
+                <SearchPanel/>
+                <ItemStatusFilter/>
+            </div>
+            <TodoList todos={todoData}/>
         </div>
-    )
+    );
 };
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>, document.getElementById('root'));
